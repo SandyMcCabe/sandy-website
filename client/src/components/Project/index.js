34a907dcd92f995c7c28e970db3998c092cba863
Sandy McCabe <sandytech13@gmail.com>
2022-01-19 09:00:43 -0600

    html for header and about

---
+++ client/src/components/Project/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Project({ project }) {
+    const { name, repo, link, description } = project;
+
+    return (
+        <div>
+            <img />
+            <div>
+                <h3></h3>
+                <p></p>
+            </div>
+        </div>
+    );
+}
+
+export default Project;
