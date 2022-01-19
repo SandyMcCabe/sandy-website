9fa88fca02586eae77ba26c185f574a37ec11666
Sandy McCabe <sandytech13@gmail.com>
2022-01-19 09:00:39 -0600

    html for header and about

---
+++ client/src/components/About/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function About() {
+    return(
+        <section> 
+            <div>
+                <p>
+                    {/* add lorem crap here later**************** */}
+                    Lorem 
+                </p>
+                <p>
+                    {/* add lorem crap here later**************** */}
+                    Lorem 
+                </p>
+                <p>
+                    {/* add my picture here too**************** */}
+                    Lorem
+                </p>
+            </div>
+        </section>
+    );
+}
+
+export default About;
---
+++ client/src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import gears from '../../assets/projects/gears.jpg';
+
+function Header() {
+    return(
+        <header className='flex-row space-between px-1'> 
+            <h1> Sandy McCabe's Website </h1>
+            <img src={gears} alt='Gears Image'> </img>
+            {props.children}
+        </header>
+    );
+}
+
+export default Header;
